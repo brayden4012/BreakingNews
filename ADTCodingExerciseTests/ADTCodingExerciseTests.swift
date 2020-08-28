d5bf35062b9c1ea58ffbe7e496cec2ce4e2cedbb
Brayden Harris <brayden4012@gmail.com>
2020-08-28 10:55:33 -0600

    Initial Commit

---
+++ ADTCodingExerciseTests/ADTCodingExerciseTests.swift
@@ -0,0 +1,34 @@
+//
+//  ADTCodingExerciseTests.swift
+//  ADTCodingExerciseTests
+//
+//  Created by Brayden Harris on 8/28/20.
+//  Copyright © 2020 Brayden Harris. All rights reserved.
+//
+
+import XCTest
+@testable import ADTCodingExercise
+
+class ADTCodingExerciseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
